7c78055df797e9bb1e5b5457de72d9302c619885
Ben Gosney <bengosney@googlemail.com>
2019-12-24 10:07:16 +0000

    Updates

---
+++ precache-manifest.c1266706c423cabba1e8e7501f87c970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6804c7c131b289483bcbcdc04935201",
+    "url": "/corridor/index.html"
+  },
+  {
+    "revision": "5136c6b86932003e444f",
+    "url": "/corridor/static/css/main.0ca95527.chunk.css"
+  },
+  {
+    "revision": "174b2238432bdf5fb287",
+    "url": "/corridor/static/js/2.ff51a027.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/corridor/static/js/2.ff51a027.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5136c6b86932003e444f",
+    "url": "/corridor/static/js/main.1eed26ca.chunk.js"
+  },
+  {
+    "revision": "c02fe49a73cb8ac431f7",
+    "url": "/corridor/static/js/runtime-main.0dd31c0f.js"
+  }
+]);
